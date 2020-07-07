c9b3150b5868bdd784d9e98abbd89481a53e7c08
danayeon <tigerwest1009@gmail.com>
2020-07-07 09:02:02 +0900

    Update method.rb

--- method.rb
+++ method.rb
@@ -1,5 +1,18 @@
 # いろいろ使えそうな自作関数
 
+# 時間の計算
+# hour = 〇〇時, minute = 〇〇分, amount = 時間差(分)
+# when = 過去か未来か, add = 0埋め2桁(nil以外ならなんでも良い)
+# timeArrange, add0 との併記が必要
+def timeCalculate(hour, minute, amount, when, add = '')
+  if when == 'f'
+    minute += amount
+  else when == 'p'
+    minute -= amount
+  end
+  return timeArrange(hour, minute, add)
+end
+
 # 時刻の調整
 # hh:mm で与えられた時刻に関して、mmの部分の超過分、不足分をhh部分を調整することで適切な表記に変える
 # 引数addに何かを指定すると下記のadd0を実行するため使用時はadd0の記述も必要
@@ -15,7 +28,7 @@ def timeArrange(hour, minute, add = '')
         hour += 1
         minute -= 60
     end
-    unless add =='n'
+    unless add == ''
         return add0(hour, minute)
     end
     return hour, minute
